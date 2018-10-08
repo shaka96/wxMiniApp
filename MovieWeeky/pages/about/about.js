277892160df107f22c0879f2bf2df07367582626
aryan <aryan86_2014@163.com>
2018-10-08 16:43:12 +0800

    wxMiniApp-Demo:MovieWeeky

---
+++ MovieWeeky/pages/about/about.js
@@ -0,0 +1,6 @@
+var aboutData = {
+    name: 'Hello World!'
+};
+
+Page({
+})
---
+++ MovieWeeky/pages/weekly/weekly.js
@@ -0,0 +1,18 @@
+// var thisWeekMoive = {
+//     name: "教父",
+//     comment: "最精彩的剧本，最真实的黑帮电影",
+//     imagePath: "/images/jf.jpg"
+// };
+
+Page({
+    data: {
+        thisWeekMoive: {
+            name: "教父",
+            comment: "最精彩的剧本，最真实的黑帮电影",
+            imagePath: "/images/jf.jpg",
+            isHighlyRecommended: true
+        },
+        count: 123,
+        score: 60
+    }
+})
